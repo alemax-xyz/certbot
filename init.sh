1cc6b5c70a565b968529c12d04ee4601142af946
Alexey U. Maksutov <alexey.maksutov@gmail.com>
2017-10-12 14:43:54 -0400

    init script fixes

--- init.sh
+++ init.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
 test -z "$PUID" && PUID=50 || test "$PUID" -eq "$PUID" || exit 2
-test -z "$PGID" && PGID=$(id -g $PUID) || test "$PGID" -eq "$PGID" || exit 2
+PUSER=$(getent passwd $PUID | cut -d: -f1)
+test -n "$PUSER" || PUSER=$(getent passwd 50 | cut -d: -f1) || usermod --uid $PUID "$PUSER" || exit 2
 
-PUSER=$(id -un $PUID 2>/dev/null)
-test -n "$PUSER" || PUSER=$(id -un 50) || usermod --uid $PUID "$PUSER" || exit 2
-
+test -z "$PGID" && PGID=$(id -g "$PUSER") || test "$PGID" -eq "$PGID" || exit 2
 PGROUP=$(getent group $PGID | cut -d: -f1)
 if [ -z "$PGROUP" ]; then
-	PGROUP=$(id -gn $PUID)
+	PGROUP=$(id -gn "$PUSER")
 	groupmod --gid $PGID "$PGROUP" || exit 2
 else
-	test $(id -g $PUID) -eq $PGID || usermod --gid $PGID "$PUSER" || exit 2
+	test $(id -g "$PUSER") -eq $PGID || usermod --gid $PGID "$PUSER" || exit 2
 fi
-
-echo $PUID $PGID $PUSER $PGROUP
 
 chown $PUID:$PGID -R /www/.well-known/acme-challenge /etc/letsencrypt /var/log/letsencrypt /var/lib/letsencrypt || exit 2
 
