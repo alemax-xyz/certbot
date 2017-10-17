c7b761f3bf0f86dd917e5e496a69c21d33b675e6
Alexey Maksutov <alexey.maksutov@mediahub.com>
2017-10-17 18:18:27 -0400

    Init script bugfix

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 test -z "$PUID" && PUID=50 || test "$PUID" -eq "$PUID" || exit 2
 PUSER=$(getent passwd $PUID | cut -d: -f1)
-test -n "$PUSER" || PUSER=$(getent passwd 50 | cut -d: -f1) || usermod --uid $PUID "$PUSER" || exit 2
+test -n "$PUSER" || PUSER=$(getent passwd 50 | cut -d: -f1) && usermod --uid $PUID "$PUSER" || exit 2
 
 test -z "$PGID" && PGID=$(id -g "$PUSER") || test "$PGID" -eq "$PGID" || exit 2
 PGROUP=$(getent group $PGID | cut -d: -f1)
